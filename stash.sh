0a0706781264976fc22c52362da961a19be56ae9
alex <alexxtreem@gmail.com>
2022-03-17 10:14:55 +0300

    Make some changes in stash.sh file, remove sensetive data

--- stash.sh
+++ stash.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 #Envirenment section
-destName='/home/alex/stashfile'
-sourceName='/mnt/s3selectel'
-fileName='main.htm'
+source /home/alex/stashfile/vars
 #End env section
 
 if [ -f $sourceName/$fileName ]
 then
 	mv $sourceName/$fileName $destName/
-	chown alex:alex $destName/$fileName
+	chown $owner $destName/$fileName
 	chmod 644 $destName/$fileName
 fi
 
