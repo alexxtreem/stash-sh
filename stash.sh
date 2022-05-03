31336b360f688196d7bb74f6665df9686130f690
alex <alexxtreem@gmail.com>
2022-05-03 16:56:13 +0300

    Add new file name for moved file

--- stash.sh
+++ stash.sh
@@ -5,8 +5,8 @@ source /home/alex/stashfile/vars
 
 if [ -f $sourceName/$fileName ]
 then
-	mv $sourceName/$fileName $destName/
-	chown $owner $destName/$fileName
-	chmod 644 $destName/$fileName
+	mv $sourceName/$fileName $destName/$newfilename
+	chown $owner $destName/$newfilename
+	chmod 644 $destName/$newfilename
 fi
 
